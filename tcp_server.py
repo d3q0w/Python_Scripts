da1abcfdef66c62220c8da5b9c09e1757e307716
D3q0w <decomohamedabdi@gmail.com>
2025-06-11 11:34:14 +0300

    tcp server code

---
+++ tcp_server.py
@@ -0,0 +1,24 @@
+import socket 
+import threading 
+bind_target="0.0.0.0"
+bind_port=99
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind((bind_target,bind_port))
+
+server.listen(5)
+
+print("[*] Listening on %s:%d" % (bind_target,bind_port))
+def handle_client(client_socket):
+    request= client_socket.recv(1024)
+    print ("[*] Received: %s " % request)
+    
+    client_socket.send("ACK")
+    client_socket.close()
+    
+while True:
+    client,addr = server.accept()
+    print("[*]Accepted connection from: %s:%d" % (addr[0],addr[1]))
+    client_handler = threading.Thread(target=handle_client,args=(client,))
+    client_handler.start()
+
